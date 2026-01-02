6dcf7be8925ef99dd1265ac56c4acea93ca9f220
Emilia Simmons <emilia.milisims@gmail.com>
2026-01-02 15:44:04 -0500

    fix: global hls not a field

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -3,7 +3,8 @@
 (string [ "\"" "'" "[[" ] @comment  [ "\"" "'" "]]" ] @comment)
 
 ((identifier) @module.builtin
-  (#any-of? @module.builtin "vim"  "mia" "P" "N" "P1" "T" "put" "keys" "values"))
+  (#any-of? @module.builtin "vim"  "mia" "P" "N" "P1" "T" "put" "keys" "values")
+  (#not-has-parent? @module.builtin field))
 
 (dot_index_expression
   table: (identifier) @_G (#eq? @_G "_G")
